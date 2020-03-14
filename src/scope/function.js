950362002ebb0e45a440a8ee51eaea702c6b37de
Jhoss <kuro.rc10@gmail.com>
2020-03-14 15:32:03 -0500

    Segundo ejemplo con function scope

--- src/scope/function.js
+++ src/scope/function.js
@@ -7,3 +7,17 @@ fruits()
 
 console.log(undefined == fruits()); // true
 console.log(undefined == fruits); // false
+
+
+// Ejemplo 2
+
+const anotherFunction = () => {
+    var x = 1;
+    var x = 2;
+    let y = 1;
+    // let y = 2;
+    console.log(x);
+    console.log(y);
+}
+
+anotherFunction();
