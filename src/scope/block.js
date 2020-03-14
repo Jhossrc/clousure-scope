94ad20b55844978e09d61e6a76136b224d431e6b
Jhoss <kuro.rc10@gmail.com>
2020-03-14 15:41:18 -0500

    Primer ejemplo de Block Scope

---
+++ src/scope/block.js
@@ -0,0 +1,18 @@
+let fruit2 = 'pineapple'
+
+const fruits = () => {
+
+    if (true) {
+        var fruit1 = 'Apple';
+        let fruit2 = 'Banana';
+        const fruit3 = 'Strawberry';
+
+        console.log(fruit3);
+    }
+
+    console.log(fruit1);
+    console.log(fruit2);
+
+}
+
+fruits();
